df7a7bf7b3570998c5a4e0628b8eaf7c93ecf858
siddharthagar229 <siddharthagarwal229@gmail.com>
2022-09-11 19:08:37 +0530

    commit

---
+++ src/useFetch.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+
+// setting the api link
+export const API_URL = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+/* plz subsribe to thapa technical channel 
+          https://www.youtube.com/thapatechnical
+         */
+
+const useFetch = (query ) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState({ show: "false", msg: "" });
+  const [movie, setMovie] = useState([]);
+
+  const getMovie = async (url) => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      console.log(data);
+      if (data.Response === "True") {
+        setIsLoading(false);
+        setMovie(data.Search || data);
+        setIsError({ show: "false", msg: "" });
+      } else {
+        setIsError({ show: "true", msg: data.Error });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // debouncing in react js
+  useEffect(() => {
+    let timeOut = setTimeout(() => {
+      getMovie(`${API_URL}&s=${query}`);
+    }, 1000);
+    console.log("set");
+    return () => {
+      clearTimeout(timeOut);
+      console.log("clear");
+    };
+  }, [query]);
+
+  return { isLoading, isError, movie };
+};
+
+export default useFetch;
